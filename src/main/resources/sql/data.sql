72a34fe2b85287c5d238548f75b0a619544b8673
saba000 <hanyubeiwai@gmail.com>
2022-11-25 15:48:08 +0900

    init repo

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,12 @@
+-- default password is password
+insert into
+    accounts(name, password)
+values
+    (
+        'username1',
+        '$2a$10$Prbd5fP7AhTZAd7mmQ9phO2gW3KFaLUNTbbKVF89qdskOFGPO/.RS'
+    ),
+    (
+        'username2',
+        '$2a$10$Prbd5fP7AhTZAd7mmQ9phO2gW3KFaLUNTbbKVF89qdskOFGPO/.RS'
+    );
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE accounts(
+    id bigint AUTO_INCREMENT,
+    name varchar(20),
+    password varchar(60)
+);
