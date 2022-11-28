bfed57d57922759d3c0f2115406a32d73f931211
saba000 <hanyubeiwai@gmail.com>
2022-11-28 13:04:05 +0900

    Rename

--- src/main/java/com/ssr/springbootjwt/web/security/authorization/JwtInValidEntryPoint.java
+++ src/main/java/com/ssr/springbootjwt/web/security/authorization/JwtInValidEntryPoint.java
@@ -1,7 +1,6 @@
 package com.ssr.springbootjwt.web.security.authorization;
 
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.springframework.http.HttpStatus;
@@ -15,16 +14,17 @@ import jakarta.servlet.http.HttpServletResponse;
 /*
 *  
 */
-public class JwtAuthorizationFailureHandler implements AuthenticationEntryPoint {
+public class JwtInValidEntryPoint implements AuthenticationEntryPoint {
 
-    private Logger logger = Logger.getLogger(JwtAuthorizationFailureHandler.class.getName());
+    private Logger logger = Logger.getLogger(JwtInValidEntryPoint.class.getName());
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
-        logger.log(Level.SEVERE, "authorization failed", authException);
+        var message = authException.getMessage();
+        logger.severe(message);
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.getWriter().write("authorization failed");
+        response.getWriter().write(message);
     }
 
 }
